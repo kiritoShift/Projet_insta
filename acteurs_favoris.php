ebdcbb91078ad0609ded6e458b361adddc89fb32
vincent voisin <vincent.voisin1986@gmail.com>
2011-12-12 13:37:47 +0100

    ajout pages

---
+++ acteurs_favoris.php
@@ -0,0 +1,26 @@
+<?session_start();?>
+<?include("connexion.php");
+include("menu_user.php");
+$choix=0;
+if(isset($_GET["choix"]))
+{
+   $choix=$_GET["choix"];
+}
+if($choix==1)
+{
+ $result = mysql_query("select * from famille order by code_famille ASC;");
+}
+else
+{
+ if($choix==2)
+ {
+  $result = mysql_query("select * from famille order by libelle ASC;");
+ }
+ else
+ { 
+  $result = mysql_query("select * from famille;"); 
+ }
+}?>
+
+<?php
+echo "acteurs favoris";
