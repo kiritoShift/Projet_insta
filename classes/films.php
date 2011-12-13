2f3e2c6bdd7b077253a0edee297d8cf2275df2c5
bobo <bobo@bobo-PC>
2011-12-13 12:33:05 +0100

    modif

--- classes/films.php
+++ classes/films.php
@@ -5,24 +5,25 @@ class films {
 	private $id_films;
 	private $titre_films;
 	private $sinopsys_films;
-	public function __construct($id_films, $nom_films, $sinopsys_films){
+	public function __construct($id_films = "", $nom_films = "", $sinopsys_films = ""){
 		
 		$this->id_film = $id_films;
 		$this->nom_films = $nom_films;
 		$this->sinopsys_films = $sinopsys_films;
 	}
 	
-	public function acteurs_new($nom_films, $sinopsys_films){
+	public function films_new($nom_films ="", $sinopsys_films =""){
+		global $conn;
 		$query = "INSERT INTO acteurs(nom_films, sinopsys_films) VALUES ('".$nom_films."','".$sinopsys_films."'))";
 		$conn->query($this->nom_films, $this->sinopsys_films);
 	}
 	
-	public function acteurs_drop($id_films){
+	public function films_drop($id_films){
 		$query = "DELETE QUICK FROM films WHERE ".$id_films." = id_films;";
 		$conn->query($this->id_films);
 	}
 	
-	public function acteur_update($id_films, $nom_films, $sinopsys_films) {
+	public function films_update($id_films, $nom_films ="", $sinopsys_films ="") {
 		if (!empty($nom_films)) {
 			$query = "UPDATE films SET nom_films = ".$nom_films." WHERE id_films = ".$id_films.";";
 			$conn->query($this->id_films, $this->nom_films);
--- films/recuperation_films_semaine.php
+++ films/recuperation_films_semaine.php
@@ -1,1 +1,44 @@
+<?php 
+include '../connection_bdd.php'; 
+include '../classes/films.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>recuperation rss</title>
+</head>
+
+<body>
 <?php
+/**
+ * 
+ * transformer le fichier XML en objet $xml
+ * 
+ */
+$xml=simplexml_load_file('http://rss.allocine.fr/ac/cine/cettesemaine.rss');
+
+/**
+ * 
+ * Mettre la liste des titres des films dans un tableau
+ * 
+ */
+$i=0;
+$tab_liste_prochaine_sortie_dvd=array("titre" =>"");
+foreach($xml->channel->item as $item){
+	$tab_liste_prochaine_sortie_dvd["titre"]=(string)$item->title;
+	var_dump($tab_liste_prochaine_sortie_dvd);
+}
+foreach ($tab_liste_prochaine_sortie_dvd as $tab) {
+	$nom_films= $tab_liste_prochaine_sortie_dvd[$i];
+	$film = new films($nom_films);
+	$film->films_new();
+}
+
+?>
+
+</body>
+</html>
