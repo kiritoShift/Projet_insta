1a383dc4b0b940f60cf1f58faf483bcbd4a78138
Jagatjit Mohinder <mohinderjagatjit.com>
2011-12-13 09:19:28 +0100

    Page Admin

--- admin.php
+++ admin.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+ 
+  <head>
+ 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+ 
+    <title>Page d'administration</title>
+  </head>
+ 
+	<body>
+	<?php
+	
+
+
+	?>
+ 	</body>
+</html>
