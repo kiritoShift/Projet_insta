2ad8d155dacc7dbf0d1a163485b24f244c31377a
bobo <bobo@bobo-PC>
2011-12-14 14:54:46 +0100

    ajout commentaire et objet

--- classes/acteurs.php
+++ classes/acteurs.php
@@ -1,35 +1,46 @@
-<?php include '../connection_bdd.php'; ?>
+<?php include 'connection_bdd.php'; ?>
 <?php
-
+/**
+ * 
+ * creation de l'objet acteurs avec des fonctions pour la modification SQL
+ * @author bobo
+ *
+ */
 class acteurs {
-	private $acteur;
+	private $id_acteur;
 	private $nom_acteur;
 	private $prenom_acteur;
-	public function __construct($id_acteur, $nom_acteur, $prenom_acteur){
+	
+	public function __construct($id_acteur= "", $nom_acteur= "", $prenom_acteur= ""){
 		
-		$this->acteur = $id_acteur;
+		$this->id_acteur = $id_acteur;
 		$this->nom_acteur = $nom_acteur;
 		$this->prenom_acteur = $prenom_acteur;
 	}
-	
-	public function acteurs_new($nom_acteur, $prenom_acteur){
-		$query = "INSERT INTO acteurs(nom_acteur, prenom_acteur) VALUES ('".$nom_acteur."','".$prenom_acteur."'))";
-		$conn->query($this->nom_acteur, $this->prenom_acteur);
+	// fonction d'ajout d'un nouveau acteur
+	public function acteurs_new(){
+		global $conn;
+		$query =$conn->prepare("INSERT INTO acteurs(nom_acteur, prenom_acteur) VALUES (:nom_acteur,:prenom_acteur)");
+		$query->execute(array("nom_acteur" =>$this->nom_acteur, "prenom_acteur" =>$this->prenom_acteur));
 	}
 	
-	public function acteurs_drop($id_acteur){
-		$query = "DELETE QUICK FROM acteurs WHERE ".$id_acteur." = id_acteur;";
-		$conn->query($this->acteur);
+	// fonction de supression d'un acteur
+	public function acteurs_drop(){
+		global $conn;
+		$query =$conn->prepare( "DELETE QUICK FROM acteurs WHERE :id_acteur = id_acteur;");
+		$query->execute(array("id_acteur" =>$this->id_acteur));
 	}
 	
-	public function acteur_update($id_acteur, $nom_acteur, $prenom_acteur) {
+	// fonction de modification d'un champ dans la table acteurs
+	public function acteur_update() {
+		global $conn;
 		if (!empty($nom_acteur)) {
-			$query = "UPDATE acteur SET nom_acteur = ".$nom_acteur." WHERE id_acteur = ".$id_acteur.";";
-			$conn->query($this->acteur, $this->nom_acteur);
+			$query =$conn->prepare("UPDATE acteur SET nom_acteur = :nom_acteur WHERE id_acteur = :id_acteur;");
+			$query->execute(array("id_acteur" =>$this->id_acteur, "nom_acteur" =>$this->nom_acteur));
 		}
 		if (!empty($nom_acteur)) {
-			$query = "UPDATE acteur SET prenom_acteur = ".$prenom_acteur." WHERE id_acteur = ".$id_acteur.";";
-			$conn->query($this->acteur, $this->prenom_acteur);
+			$query =$conn->prepare("UPDATE acteur SET prenom_acteur = :prenom_acteur WHERE id_acteur = :id_acteur;");
+			$query->execute(array("id_acteur" =>$this->id_acteur,"prenom_acteur" =>$this->prenom_acteur));
 		}
 	}
 }
--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php include 'connection_bdd.php'; ?>
+<?php include 'classes/acteurs.php'; ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <?php 
+/*
 $test = "toto";
 echo $test;
 
@@ -13,9 +15,13 @@ while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
 	print_r($row);print "<br />";
 }
 
-
-
 $result = mysql_query("select * from films order by titre_films ASC;");
 echo $result;
+*/
+$nom_acteur="bobo";
+$prenom_acteur="toto";
+$id="4";
+$film = new acteurs($id, $nom_acteur, $prenom_acteur);
+$film->acteurs_drop();
 
 ?>
