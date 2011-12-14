49c99c573bbd7f428102765028cfe27572af9491
bobo97442 <couraud.boris@gmail.com>
2011-12-14 12:10:31 +0100

    Update fonction.php

--- fonction.php
+++ fonction.php
@@ -1,1 +1,11 @@
-<?php
+<?php include 'connection_bdd.php'; ?>
+<?php 
+
+function liste_films() {
+  global $conn;
+	$query = $conn->prepare("SELECT * FROM films ORDER BY titre_films ASC;");
+	$query->execute();
+	$liste_films = $query->fetchAll(PDO::FETCH_ASSOC);
+	return $liste_films;
+}
+?>
