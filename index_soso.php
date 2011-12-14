e4f460a82e6ad0521bba7a1042337e2f9ffe6ed6
loupac <loupac@172.16.135.130>
2011-12-14 10:17:56 +0100

    ajout image et indextest

---
+++ index_soso.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+       <title>LOUPAC-PROD CORPORATION</title>
+       <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+       <link rel="stylesheet" media="screen" type="text/css" title="loupac" href="acceuil.css" />
+
+   </head>
+
+    <body>
+
+      <?php
+
+        include "acceuil.php";
+
+      ?>   
+      <input type="submit" name="btninscription" value="Inscription" href="formulaire_inscription.php" onclick="window.open('formulaire_inscription.php', 'exemple', 'height=800%, width=800, top=90, left=350, toolbar=no, menubar=no, location=yes, resizable=yes, scrollbars=yes, status=no'); return false;"/> 
+		
+<!--
+      <iframe src="http://localhost/form1-corrige%281%29.php" height="300" width="300" scfrolling="no"></iframe> 
+-->
+    </body>
+</html>
+
+
+
+
