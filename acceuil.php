e3fa4817cd9ab053a9290f47aa5ce62ac07737ac
loupac <loupac@172.16.135.130>
2011-12-16 10:33:29 +0100

    ok

--- acceuil.php
+++ acceuil.php
@@ -1,34 +1,17 @@
 <?php include "connection_bdd.php" ?>
 
 <body>
-<a class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4be01d572857270c"><img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:0"/></a><script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4be01d572857270c"></script>
-</body>
-
-
-
-<div id="bla">
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
-blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+<a id="atbtop" class="addthis_button" href="http://www.addthis.com/bookmark.php?v=250&amp;username=xa-4be01d572857270c">
+		<img src="http://s7.addthis.com/static/btn/v2/lg-share-en.gif" width="125" height="16" alt="Bookmark and Share" style="border:none;" />
+	</a>
+	<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=xa-4be01d572857270c"></script>
+<div id="bando">
+	
+	<img src="images/bando.jpg" border="0" width="780px" height="180px" />
 </div>
 
-<div id="bouton_souscrire"></div><input type="submit" name="btninscription" value="Inscription" href="formulaire_inscription.php" onclick="window.open('formulaire_inscription.php', 'exemple', 'height=800%, width=800, top=90, left=350, toolbar=no, menubar=no, location=yes, resizable=yes, scrollbars=yes, status=no'); return false;"/> 
+
+<div id="bouton_souscrire"><input type="submit" name="btninscription" value="Inscription" href="formulaire_inscription.php" onclick="window.open('formulaire_inscription.php', 'exemple', 'height=800%, width=800, top=90, left=350, toolbar=no, menubar=no, location=yes, resizable=yes, scrollbars=yes, status=no'); return false;"/> 
 </div>		
    
 <ul id="menuDeroulant">
@@ -68,3 +51,34 @@ blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
  </li>
 
 </ul>
+
+
+
+<div id="bla">
+<br><br><br><br><br><br><br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+blablaihpsfduhpufhpuhfgpouhfoguhspofhgqoohig<br>
+</div>
+
+
+
+
+
+</body>
