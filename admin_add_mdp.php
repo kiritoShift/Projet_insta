7681584e9fa1a464648c9468bfd5154f3218c57c
Jagatjit Mohinder <mohinderjagatjit.com>
2011-12-17 15:06:46 +0100

    maj

--- admin_add_mdp.php
+++ admin_add_mdp.php
@@ -4,17 +4,94 @@
  
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
  
-    <title>Page d'ajout MDP</title>
+    <title>Page modification MDP</title>
+    <link rel="stylesheet" href="css/admin.css" />
     
   </head>
  
 	<body>
-	<h2 align=center> Bienvenue sur la page d'ajout de mot de passe</h2>
+	<h2 align=center> Page de modification des mots de passe utilisateurs</h2>
 	
 	
-	<?php 
-	?>
+	<?php include 'classes/mot_de_passe.php';
+
+		if (empty($_POST['id_users']) || empty($_POST['confirmation_Mot_de_passe']) || empty($_POST['Mot_de_passe'])) {
+			  		if (!empty($_POST['btnEnvoyer'])) {
+			  			echo '<div id="messageerror">Merci de remplir les champs oblgatoire </div>';
+			  					}
 	
+	echo '
+  				
+    			
+				<form method="POST">
+				
+<!--*******************************************************************************************************************************************-->				
+<!--***********************premier encadr� information personnelle (non, prenom, date de naissance, civiliter)*********************************-->
+<!--*******************************************************************************************************************************************-->						
+				
+				
+							<fieldset class="formulaire" style="width:350px;">
+							<legend> Informations utilisateur </legend>
+				
+				
+
+  									<!--** id **-->
+
+  									
+								<label for="id_users">ID<FONT color="red">*</FONT> :</label>
+								<input type="text" id="id_users" name="id_users"/>
+								<br />
+								<br />					
+
+																
+
+	  								<!--********** Mot de passe ************-->
+
+						
+								<label for="IDMdp">Mot de passe<FONT color="red">*</FONT> :</label>
+								<input type="password" id="IDMdp" name="mdp"/>
+								<br />
+								<br />
+																
+								
+
+	  								<!--**** confirmation Mot de passe *****-->
+
+						
+								<label for="IDCmdp">confirmation du Mot de passe<FONT color="red">*</FONT> :</label>
+								<input type="password" id="IDCmdp" name="confirmation_Mot_de_passe"/>
+								<br />
+								<br />
+							
+								
+								
+								</fieldset>				
+						
+
+
+								<!--** Bouton Envoyer **-->
+								
+							    </fieldset>
+								<br />
+								 <div id="test"><input class="condition" type="submit" name="btnEnvoyer" value="Envoyer" /><div>
+								<br />
+								<br />
+						
+						';
+				  	}
+
+	else {
+	 		$envoie_form = new utilisateurs($_POST['id_users'], $_POST['mdp']);	
+	 		$envoie_form->users_new();
+	 		
+	 		echo "L'utilisateur " .$_POST['nom_users']." ".$_POST['prenom_users']. " a bien été ajouter à la base de données";
+	 		
+  		
+	}
+  			
+	
+  		
+?>
 	
  	</body>
 </html>
--- admin_del_mdp.php
+++ admin_del_mdp.php
@@ -4,17 +4,92 @@
  
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
  
-    <title>Page suppression MDP</title>
+    <title>Page supression MDP</title>
+    <link rel="stylesheet" href="css/admin.css" />
     
   </head>
  
 	<body>
-	<h2 align=center> Bienvenue sur la page de supression de mot de passe utilisateur</h2>
+	<h2 align=center> Page de supression des mots de passe utilisateurs</h2>
 	
 	
-	<?php 
-	?>
+	<?php include 'classes/mot_de_passe.php';
+
+		if (empty($_POST['id_mdp']) || empty($_POST['confirmation_Mot_de_passe']) || empty($_POST['Mot_de_passe'])) {
+			  		if (!empty($_POST['btnEnvoyer'])) {
+			  			echo '<div id="messageerror">Merci de remplir les champs oblgatoire </div>';
+			  					}
+
 	
+	echo '
+  				
+    			
+				<form method="POST">
+				
+	
+				
+							<fieldset class="formulaire" style="width:350px;">
+							<legend> Informations utilisateur </legend>
+				
+				
+
+  									<!--** id **-->
+
+  									
+								<label for="id_mdp">ID<FONT color="red">*</FONT> :</label>
+								<input type="text" id="id_mdp" name="id_mdp"/>
+								<br />
+								<br />					
+
+																
+
+	  								<!--********** Mot de passe ************-->
+
+						
+								<label for="mdp">Mot de passe<FONT color="red">*</FONT> :</label>
+								<input type="password" id="mdp" name="mdp"/>
+								<br />
+								<br />
+																
+								
+
+	  								<!--**** confirmation Mot de passe *****-->
+
+						
+								<label for="mdp">confirmation du Mot de passe<FONT color="red">*</FONT> :</label>
+								<input type="password" id="mdp" name="mdp"/>
+								<br />
+								<br />
+							
+								
+								
+								</fieldset>				
+						
+
+
+								<!--** Bouton Envoyer **-->
+								
+							    </fieldset>
+								<br />
+								 <div id="test"><input class="condition" type="submit" name="btnEnvoyer" value="Envoyer" /><div>
+								<br />
+								<br />
+						
+						';
+				  	}
+
+	else {
+	 		$envoie_form = new mot_de_passe($_POST['id_users'], "");	
+	 		$envoie_form->mot_de_passe_delete();
+	 		
+	 		echo "Le mot de passe a bien été supprimer de la base de données";
+	 		
+  		
+	}
+  			
+	
+  		
+?>
 	
  	</body>
 </html>
