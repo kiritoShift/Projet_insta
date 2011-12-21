f6085d10bf79689cc74bc66cfcd0bbc88a4d512b
bobo <bobo@bobo-PC>
2011-12-21 17:46:31 +0100

    maj

--- Liste_films_acceuil_cine.php
+++ Liste_films_acceuil_cine.php
@@ -11,12 +11,13 @@ $query->execute(array());
 $tab_sortir= $query->fetchAll(PDO::FETCH_OBJ);
 
 foreach ($tab_sortir as $tab){
-	
 	$body=" 
 <p align='center'>_________________________________________________________</p><br />
 <p align='center'> ".$tab->titre_films."</p><br />
 <P align='center'><img height='400' width='300' src='".$tab->jaquette_films."' alt='Pas d'image disponible'></p><br />
 <p align='center'> Date de sortie : ".$tab->date_sortie."</p><br />
+<p align='center'> <a href='favoris_add.php?id=".$tab->id_films." title='supprimer favoris'><img src='images/etoile_favoris.gif' alt='iconePoubelle' border='0'></a></p><br />
+<p align='center'> Sinopsys : ".$tab->sinopsys_films."</p><br />
 	";
 	
 	$date_limite=date( "Y-m-d");
