6b5bb8d9643ca22eeb636ff14c2f087374ccad08
vincent voisin <vincent.voisin1986@gmail.com>
2011-12-21 14:39:23 +0100

    modif

--- favoris_del.php
+++ favoris_del.php
@@ -3,8 +3,7 @@ include_once ("connexion_bdd.php");
 include("fonction.php");
 spl_autoload_register('autoClass_racine');
 
-
-$new=new avoir_films_favoris("id_users", $_GET['id']);
+$new=new avoir_films_favoris($_SESSION['pseudo_users'], $_GET['id']);
 $new->avoir_films_favoris_delete();
 
 
