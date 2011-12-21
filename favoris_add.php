a8dc7c96f8ae641e027ae10f1ff8fd6b42667960
vincent voisin <vincent.voisin1986@gmail.com>
2011-12-21 14:21:25 +0100

    ajout

---
+++ favoris_add.php
@@ -0,0 +1,15 @@
+<?php
+include_once ("connexion_bdd.php");
+include("fonction.php");
+spl_autoload_register('autoClass_racine');
+
+$new=new avoir_films_favoris("id_users", $_GET['id']);
+$new->avoir_films_favoris_new();
+
+
+?>
+
+<html>
+   <META http-equiv="Refresh" content = "10;URL=page_user.php">
+
+</html>
